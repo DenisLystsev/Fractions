ddcb4d64aa61ea2d84d2eca877317650ddab37d2
Denis Lystsev <dlyst@mail.ru>
2018-08-07 14:18:56 +0300

    Разработал класс Fractions. Добавил действия над дробями: умножение, деление. Рабочая программа ver 0.4.0

--- Fractions/Form1.Designer.cs
+++ Fractions/Form1.Designer.cs
@@ -28,97 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tbNumerator1 = new System.Windows.Forms.TextBox();
+            this.tbDenominator1 = new System.Windows.Forms.TextBox();
+            this.tbNumerator2 = new System.Windows.Forms.TextBox();
+            this.tbDenominator2 = new System.Windows.Forms.TextBox();
+            this.tbDenominatorResult = new System.Windows.Forms.TextBox();
+            this.tbNumeratorResult = new System.Windows.Forms.TextBox();
+            this.cbJob = new System.Windows.Forms.ComboBox();
+            this.btnResult = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbNumerator1
             // 
-            this.textBox1.Location = new System.Drawing.Point(224, 52);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(30, 20);
-            this.textBox1.TabIndex = 0;
+            this.tbNumerator1.Location = new System.Drawing.Point(224, 71);
+            this.tbNumerator1.Name = "tbNumerator1";
+            this.tbNumerator1.Size = new System.Drawing.Size(30, 20);
+            this.tbNumerator1.TabIndex = 0;
             // 
-            // textBox2
+            // tbDenominator1
             // 
-            this.textBox2.Location = new System.Drawing.Point(224, 91);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(30, 20);
-            this.textBox2.TabIndex = 1;
+            this.tbDenominator1.Location = new System.Drawing.Point(224, 91);
+            this.tbDenominator1.Name = "tbDenominator1";
+            this.tbDenominator1.Size = new System.Drawing.Size(30, 20);
+            this.tbDenominator1.TabIndex = 1;
             // 
-            // textBox3
+            // tbNumerator2
             // 
-            this.textBox3.Location = new System.Drawing.Point(305, 52);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(30, 20);
-            this.textBox3.TabIndex = 2;
+            this.tbNumerator2.Location = new System.Drawing.Point(305, 71);
+            this.tbNumerator2.Name = "tbNumerator2";
+            this.tbNumerator2.Size = new System.Drawing.Size(30, 20);
+            this.tbNumerator2.TabIndex = 2;
             // 
-            // textBox4
+            // tbDenominator2
             // 
-            this.textBox4.Location = new System.Drawing.Point(305, 91);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(30, 20);
-            this.textBox4.TabIndex = 3;
+            this.tbDenominator2.Location = new System.Drawing.Point(305, 91);
+            this.tbDenominator2.Name = "tbDenominator2";
+            this.tbDenominator2.Size = new System.Drawing.Size(30, 20);
+            this.tbDenominator2.TabIndex = 3;
             // 
-            // textBox5
+            // tbDenominatorResult
             // 
-            this.textBox5.Location = new System.Drawing.Point(364, 91);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(30, 20);
-            this.textBox5.TabIndex = 4;
+            this.tbDenominatorResult.Location = new System.Drawing.Point(423, 91);
+            this.tbDenominatorResult.Name = "tbDenominatorResult";
+            this.tbDenominatorResult.ReadOnly = true;
+            this.tbDenominatorResult.Size = new System.Drawing.Size(30, 20);
+            this.tbDenominatorResult.TabIndex = 4;
             // 
-            // textBox6
+            // tbNumeratorResult
             // 
-            this.textBox6.Location = new System.Drawing.Point(364, 52);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(30, 20);
-            this.textBox6.TabIndex = 5;
+            this.tbNumeratorResult.Location = new System.Drawing.Point(423, 71);
+            this.tbNumeratorResult.Name = "tbNumeratorResult";
+            this.tbNumeratorResult.ReadOnly = true;
+            this.tbNumeratorResult.Size = new System.Drawing.Size(30, 20);
+            this.tbNumeratorResult.TabIndex = 5;
             // 
-            // label1
+            // cbJob
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(341, 71);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 17);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "=";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "+",
-            "-",
+            this.cbJob.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbJob.FormattingEnabled = true;
+            this.cbJob.Items.AddRange(new object[] {
             "*",
-            "/",
-            "<"});
-            this.comboBox1.Location = new System.Drawing.Point(260, 67);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(39, 24);
-            this.comboBox1.TabIndex = 7;
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            "/"});
+            this.cbJob.Location = new System.Drawing.Point(260, 71);
+            this.cbJob.Name = "cbJob";
+            this.cbJob.Size = new System.Drawing.Size(39, 24);
+            this.cbJob.TabIndex = 7;
+            this.cbJob.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(342, 72);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 8;
+            this.btnResult.Text = "Результат";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.cbJob);
+            this.Controls.Add(this.tbNumeratorResult);
+            this.Controls.Add(this.tbDenominatorResult);
+            this.Controls.Add(this.tbDenominator2);
+            this.Controls.Add(this.tbNumerator2);
+            this.Controls.Add(this.tbDenominator1);
+            this.Controls.Add(this.tbNumerator1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -128,14 +127,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox tbNumerator1;
+        private System.Windows.Forms.TextBox tbDenominator1;
+        private System.Windows.Forms.TextBox tbNumerator2;
+        private System.Windows.Forms.TextBox tbDenominator2;
+        private System.Windows.Forms.TextBox tbDenominatorResult;
+        private System.Windows.Forms.TextBox tbNumeratorResult;
+        private System.Windows.Forms.ComboBox cbJob;
+        private System.Windows.Forms.Button btnResult;
     }
 }
 
--- Fractions/Program.cs
+++ Fractions/Program.cs
@@ -17,6 +17,8 @@ namespace Fractions
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //Fractions First = new Fractions(int.Parse(tb));
         }
     }
 }
