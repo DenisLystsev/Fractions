775bbc2873ff6af60545001c9796850be6eeebc0
Denis Lystsev(Job) <dlyst@mail.ru>
2018-08-08 10:43:36 +0300

    Добавлен метод сокращения дроби GetReductFraction() в классе Fractions. Он подключен к четырем методам вычислений

--- Fractions/Fractions.cs
+++ Fractions/Fractions.cs
@@ -18,36 +18,68 @@ namespace Fractions
 
         public Fractions PlusFractions(Fractions B)
         {
+            Fractions Result;
+
             this.numerator = this.numerator * B.denominator + B.numerator * this.denominator;
             this.denominator *= B.denominator;
 
-            return this;
+            Result = this.GetReductFraction();
+
+            return Result;
         }
 
         public Fractions SubstractFractions(Fractions B)
         {
+            Fractions Result;
+
             this.numerator = this.numerator * B.denominator - B.numerator * this.denominator;
             this.denominator *= B.denominator;
+
+            Result = this.GetReductFraction();
+
+            return Result;
+        }
+
+        Fractions GetReductFraction()
+        {
+            int gcd = 0, min = 0;
+
+            min = this.numerator;
+            if (min > this.denominator) min = this.denominator;
+
+            for(int i = 1; i<=min; i++)
+            {
+                if (this.numerator % i == 0 && this.denominator % i == 0) gcd = i;
+            }
+
+            this.numerator /= gcd;
+            this.denominator /= gcd;
 
             return this;
         }
 
-
-
         public Fractions MultiFractions(Fractions B)
         {
+            Fractions Result;
+
             this.numerator *= B.numerator;
             this.denominator *= B.denominator;
 
-            return this;
+            Result = this.GetReductFraction();
+
+            return Result;
         }
 
         public Fractions DividingFractions(Fractions B)
         {
+            Fractions Result;
+
             this.numerator *= B.denominator;
             this.denominator *= B.numerator;
 
-            return this;
+            Result = this.GetReductFraction();
+
+            return Result;
         }
 
         public string GetNumerator
